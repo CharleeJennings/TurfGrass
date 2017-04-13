625c80a77b48885175058141fd5a988778b476ab
mslighthouse <maxwelljsmith@rocketmail.com>
2017-04-13 01:52:41 -0500

    Add more information panels to dashboard
    Includes heat units daily, weekly, average high/low, current conditions
    weather report, and more

--- dashboard/js/forecast.js
+++ dashboard/js/forecast.js
@@ -3,11 +3,9 @@ var dateList = [];
 var dataList = [];
 var forecasts = [];
 var key = "59e5c83564468ec7e2ca593eff7e907c";
-var lat = 39.0968;
-var long = -120.0324;
-
+var lat = 32.5933574;
+var long = -85.4951663;
 var today = new Date(); // today's date
-
 function forecast(date, tempMin, tempMax, icon) {
     this.date = date;
     this.tempMin = tempMin;
@@ -18,6 +16,7 @@ function forecast(date, tempMin, tempMax, icon) {
 function convertFtoC(tempF) {
     return (tempF - 32.0) * (5 / 9)
 };
+// FORECAST
 $(window.document).ready(function () {
     // Get Forecast, display in main dashboard
     $.ajax({
@@ -28,17 +27,30 @@ $(window.document).ready(function () {
             var tempMax = Math.round(received.daily.data[0].temperatureMax);
             var tempMin = Math.round(received.daily.data[0].temperatureMin);
             var image_src = '<img id="svgimg" src="images/weather_icon/' + weather_icon + '.svg" width="80">';
-            document.getElementById("forecast-summary").innerHTML = image_src + " " + tempMax + "° - " + tempMin + "° ";
-            document.getElementById("forecast-summary-minutely").innerHTML = received.minutely.summary;
+            document.getElementById("forecast-summary").innerHTML = image_src + "<h2> " + tempMax + "° - " + tempMin + "°</h2> ";
+            document.getElementById("forecast-summary-minutely").innerHTML = Math.round(received.currently.temperature) + "° " + received.minutely.summary;
             document.getElementById("forecast-summary-hourly").innerHTML = "<h3>" + received.hourly.summary + "</h3>";
+            // variables for weekly information
+            var avg_tempMax = 0;
+            var avg_tempMin = 0;
+            var net_hu = 0;
             // get daily 1-6 for weekly forecast
             for (i = 1; i <= 6; i++) {
+                net_hu += received.daily.data[i].temperatureMax;
+                avg_tempMax += received.daily.data[i].temperatureMax;
+                avg_tempMin += received.daily.data[i].temperatureMin;
                 var dat = new Date(); // day we're working with
                 dat.setDate(today.getDate() + i);
-                var icon = '<center><img id="svgimg" src="images/weather_icon/' + received.daily.data[i].icon + '.svg" width="80"><br><h4>' + Math.round(received.daily.data[i].temperatureMax) + ' - ' + Math.round(received.daily.data[i].temperatureMin) + '</h4></center>';
+                var icon = '<center><img id="svgimg" src="images/weather_icon/' + received.daily.data[i].icon + '.svg" width="80"><br><h4>' + Math.round(received.daily.data[i].temperatureMax) + '° - ' + Math.round(received.daily.data[i].temperatureMin) + '°</h4></center>';
                 document.getElementById("day" + i).innerHTML = '<center>' + (dat.getMonth() + 1) + '-' + dat.getDate() + '</center>';
                 document.getElementById("forecast" + i).innerHTML = icon;
             }
+            avg_tempMax /= 6;
+            avg_tempMin /= 6;
+            document.getElementById("daily-heat-unit").innerHTML = Math.round(convertFtoC(received.daily.data[0].temperatureMax));
+            document.getElementById("daily-high-low").innerHTML = Math.round(avg_tempMax) + "° / " + Math.round(avg_tempMin) + "°";
+            document.getElementById("weekly-heat-unit").innerHTML = Math.round(convertFtoC(net_hu));
+            
             // If there are alerts for the area, Create them as alert panels
             if (received.alerts) {
                 received.alerts.forEach(function (alert) {
